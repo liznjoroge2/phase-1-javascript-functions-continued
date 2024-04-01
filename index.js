375440e2ffe2fb5f1f34d796729799126166b1af
enjoroge <njrogeelizabeth53@gmail.com>
2024-04-01 15:16:28 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun());
+  console.log(saturdayFun("watch movies"));
+  
+  const add = function (x, y) {
+    return x + y;
+  };
+  
+  console.log(add(2, 3));
+  
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  console.log(mondayWork("work from home"));
+  
+  (function (baseNumber) { 
+    console.log(baseNumber + 3);
+  })(2);
+  
+  console.log(saturdayFun("watch movies"));
+  
+  console.log(mondayWork("work from home"));
+  
+  function wrapAdjective(visualFlair = "*") {
+    return function (adjective) {
+      return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer"));
+  
